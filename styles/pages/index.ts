a02958106cbfdacb7a2172d30636eb9111994682
Miguel Penha <miguel.penha1@gmail.com>
2024-04-01 20:15:34 -0300

    Update index.ts

--- styles/pages/index.ts
+++ styles/pages/index.ts
@@ -3,6 +3,7 @@ import styled from 'styled-components'
 export const Container = styled.main`
     height: 100vh;
     display: flex;
+    overflow: hidden;
     overflow-y: hidden;
     background-color: ${props => props.theme.secondaryColor};
 `
